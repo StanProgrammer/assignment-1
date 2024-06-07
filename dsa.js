b71422c82299962c9abb7c93303582f6ea764120
Atib Khan <atibkhan392@gmail.com>
2024-06-07 12:15:37 +0530

    Fixed edge case of dsa

--- dsa.js
+++ dsa.js
@@ -1,4 +1,4 @@
-// Strategy :Greedy approach. At each airport, hire a plane to reach the farthest airport possible with the current fuel.
+// Strategy: Greedy approach. At each airport, hire a plane to reach the farthest airport possible with the current fuel.
 
 const minPlanes = function(fuel) {
     let numOfPlanes = 0, currFuel = 0, maxReach = 0;
@@ -15,11 +15,19 @@ const minPlanes = function(fuel) {
             numOfPlanes++;
             // Update the current fuel to the farthest airport we can reach
             currFuel = maxReach;
+
+            // If we cannot move further from the current airport, return -1
+            if (currFuel <= airport) {
+                return -1;
+            }
         }
     }
-    return numOfPlanes;
+
+    // If we end up reaching or surpassing the last airport, return the number of planes
+    return currFuel >= numAirports - 1 ? numOfPlanes : -1;
 };
 
 // Test cases
 console.log(minPlanes([2, 1, 2, 3, 1]));  // Output: 2
 console.log(minPlanes([1, 6, 3, 4, 5, 0, 0, 0, 6]));  // Output: 3
+console.log(minPlanes([1, 0, 1]));  // Output: -1
